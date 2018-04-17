5627aef2b1a1e688ce672b9eadb81800517fe0dd
elpinal <elpinal@users.noreply.github.com>
2018-04-17 17:49:53 +0900

    Add Program and Display

--- haskell/src/ELT0/Program.hs
+++ haskell/src/ELT0/Program.hs
@@ -28,3 +28,34 @@ data Inst
   | Shl Reg Operand Operand
   | Shr Reg Operand Operand
   deriving (Eq, Show)
+
+newtype Program = Program [Inst]
+
+class Display a where
+  display :: a -> String
+
+instance Display Reg where
+  display (Reg n) = 'R' : show n
+
+instance Display Val where
+  display (Word w) = show w
+
+instance Display Operand where
+  display (Register r) = display r
+  display (Value v) = display v
+
+instance Display Inst where
+  display = foldl1 (\x y -> x ++ " " ++ y) . display'
+
+display' :: Inst -> [String]
+display' (Mov r o)     = ["mov", display r, display o]
+display' (Add r o1 o2) = ["add", display r, display o1, display o2]
+display' (Sub r o1 o2) = ["sub", display r, display o1, display o2]
+display' (And r o1 o2) = ["and", display r, display o1, display o2]
+display' (Or  r o1 o2) = ["or" , display r, display o1, display o2]
+display' (Not r o)     = ["not", display r, display o]
+display' (Shl r o1 o2) = ["shl", display r, display o1, display o2]
+display' (Shr r o1 o2) = ["shr", display r, display o1, display o2]
+
+instance Display Program where
+  display (Program is) = foldr (\i s -> display i ++ "\n" ++ s) "" is
