c052c54182af8178852ddb28d8950892e323f1d5
elpinal <elpinal@users.noreply.github.com>
2018-05-03 15:59:53 +0900

    Test 'mov' and 'halt' instructions

--- haskell/test/ELT0/EvalSpec.hs
+++ haskell/test/ELT0/EvalSpec.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE BinaryLiterals #-}
+
 module ELT0.EvalSpec where
 
 import Test.Hspec
@@ -11,3 +13,5 @@ spec = do
   describe "run" $
     it "evaluates a code, then returns the result register file" $ do
       run (code [10]) `shouldBe` Map.empty
+      run (code [0b00100000, 45, 0, 0, 0, 1, 10]) `shouldBe` Map.singleton 45 1
+      run (code [0b00100000, 45, 0, 0, 0, 1, 0, 76, 45, 10]) `shouldBe` Map.fromList [(45, 1), (76, 1)]
